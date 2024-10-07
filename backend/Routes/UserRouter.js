2c8bdfd935a83d30357a9b9dcf090fad26c749ef
jaswanthinvally <jaswanthvenkat001@gmail.com>
2024-10-07 08:25:34 +0530

    jtw and bcrypt implemented

--- backend/Routes/UserRouter.js
+++ backend/Routes/UserRouter.js
@@ -1,41 +1,75 @@
 const express = require('express')
 const { UserModel } = require('../Models/UserModels')
+const bcrypt = require('bcrypt')
+const jsonwebtoken = require('jsonwebtoken')
+
 const app = express()
 const UserRouter = express.Router()
 
 
+app.use(express.json())
 
-UserRouter.route("/user/signup").post((req, res) => {
+UserRouter.route("/user/signup").post(async (req, res) => {
     const { name, email, password } = req.body
 
     if (!name || !email || !password) {
-        res.json({
-            message : "please enter the required data"
+        return res.status(400).json({
+            message: "Please enter the required data"
         })
     }
 
-    else {
-        const CreateUser = UserModel.create({
+    try {
+        const hashedpassword = await bcrypt.hash(password, 10)
+        const newUser = await UserModel.create({
             name,
             email,
-            password
+            password: hashedpassword
         })
-        CreateUser.then(() => res.send(`welcome ${name}`)).catch(() => res.send(" user creation failed"))
+
+        res.status(201).send(`Welcome ${name}`)
+    } catch (err) {
+        res.status(500).json({
+            message: "User creation failed",
+            error: err.message
+        })
     }
-    
 })
 
-UserRouter.route("/user/signin").post((req, res) =>
-    res.json({
-        message: "this is the signin route"
-    }))
+UserRouter.route("/user/signin").post(async (req, res) => {
+    const { email, password } = req.body
 
-UserRouter.route("/user/courses").get((req, res) =>
-    res.json(
-        {
-            message: "course end point"
+    if (!email || !password) {
+        return res.status(400).json({ message: "Please enter email and password" })
+    }
+
+    try {
+        const user = await UserModel.findOne({ email })
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
         }
-    ))
 
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" })
+        }
+
+        const token = jsonwebtoken.sign({ id: user._id }, 'secretKey', { expiresIn: '1h' })
+
+        res.status(200).json({
+            message: "Login successful",
+            token
+        })
+    } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message })
+    }
+})
+
+UserRouter.route("/user/courses").get((req, res) => 
+    res.json({
+        message: "Course endpoint"
+    })
+)
 
 module.exports = UserRouter
